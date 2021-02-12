460658b283c33824aa2bf3fc2a98468db97a3a0c
umuturunc <umut.trnc@gmail.com>
2021-02-12 21:30:05 +0300

    Initial commit

---
+++ src/main/java/lakresmigo/loginregister/Loginregister.java
@@ -0,0 +1,17 @@
+package lakresmigo.loginregister;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Loginregister extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
